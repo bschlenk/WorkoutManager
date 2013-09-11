906bd342611b810d9d86d6adbffc4ee18e5e6db1
Brian Schlenker <bschlenk@umich.edu>
2013-09-11 17:31:17 -0400

    NOW it works
    initial commit didn't actually work OO'''

--- workout_manager.py
+++ workout_manager.py
@@ -2,21 +2,21 @@
 import json
 import os
 
-decodeJsonFile(file_name):
+def decodeJsonFile(file_name):
 	with open(file_name) as f:
 		data = f.read()
 
 	decoder = json.JSONDecoder()
 	return decoder.decode(data)
 
-promptWithDefault(message, default_value=None):
+def promptWithDefault(message, default_value=None):
 	prompt = '%s [%s]' % (str(default_value) if default_value else '*required*')
 	return raw_input(prompt)
 	
 def main():
 	#TODO error checking
 	workout_files = os.listdir('workouts')
-	workouts = decodeJsonFile(workout_files[0])['workouts'])
+	workouts = decodeJsonFile(os.path.join('workouts', workout_files[0]))['workouts']
 	print workouts
 	
 
