adab15e07d5bd255dde119cb421b5617fdc754a9
Brian Schlenker <bschlenk@umich.edu>
2013-09-11 20:39:19 -0400

    changed format of workout json, refactored
    The format for workouts has been simplified, I am going with
    a single workout per file. The logic has been refactored, and
    the summary report at the end is formatted to be nicer.

--- workout_manager.py
+++ workout_manager.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import sys, os
 import json
-import os
+
+WORKOUT_DIR = 'workouts'
 
 def decodeJsonFile(file_name):
 	with open(file_name) as f:
@@ -9,17 +11,61 @@ def decodeJsonFile(file_name):
 	decoder = json.JSONDecoder()
 	return decoder.decode(data)
 
-def promptWithDefault(message, default_value=None):
-	prompt = '%s [%s]' % (str(default_value) if default_value else '*required*')
-	return raw_input(prompt)
+
+def promptWithDefault(message, default=None):
+	prompt = '%s [%s]: ' % (message, str(default) if default is not None else '*required*')
+	answer = raw_input(prompt)
+	return answer or default
+
+
+def promptSelectionFromList(choices, default=0, prompt='please choose an option'):
+	for i, choice in enumerate(choices):
+		print '%d: %s' % (i, str(choice))
+	print
+	choice = None
+	while choice is None:
+		try:
+			choice = int(promptWithDefault(prompt, default))
+		except ValueError:
+			print 'boowahhhh, choice must be in range 0-%d' % len(choices)
+			continue
+		if choice not in range(len(choices)):
+			print 'wise guy, choice must be in range 0-%d' % len(choices)
+			choice = None
+	return choice
+
+
+def getWorkouts(directory=WORKOUT_DIR):
+	workout_files = os.listdir(directory)
+	workouts = []
+	for workout in (decodeJsonFile(os.path.join(directory, w))['workout'] for w in workout_files):
+		workouts.append(workout)
+	workouts.sort(key=lambda a: a['name'])
+	return workouts
+	
 	
 def main():
-	#TODO error checking
-	workout_files = os.listdir('workouts')
-	workouts = decodeJsonFile(os.path.join('workouts', workout_files[0]))['workouts']
-	print workouts
-	
+	workouts = getWorkouts()
+
+	choice = promptSelectionFromList([w['name'] for w in workouts])
+	workout = workouts[choice]
+
+	rep_history = []
+	for activity in workout['activities']:
+		reps = int(raw_input('%s: ' % activity['name']))
+		rep_history.append((activity['name'], reps))
+
+	print
+	print 'Done! Printing results...'
+
+	spacing = max(map(len, [a[0] for a in rep_history]))
+	print spacing
+	for activity in rep_history:
+		print '{0:{2}}: {1}'.format(*(activity + (spacing,)))
 
 if __name__ == '__main__':
-	main()
+	try:
+		main()
+	except KeyboardInterrupt:
+		print '\nexiting...'
 
