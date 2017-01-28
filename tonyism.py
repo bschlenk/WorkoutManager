4c4dbe1be6d1344fa42d29fd3670bccb6465dc8c
Brian Schlenker <bschlenk@umich.edu>
2017-01-28 13:26:51 -0800

    Add tonyism module
    The tonyism module provides methods for getting a random tonyism. The
    possible tonyisms are stored in tonyisms.json.

--- tonyism.py
+++ tonyism.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
+
 import sys, os
-import requests
+import json
+import random as rand
 
-TONYISMS = [
+_module_dir = os.path.realpath(os.path.dirname(__file__))
+_default_path = os.path.join(_module_dir, 'tonyisms.json')
+_TONYISMS = None
 
-]
+def get(source=_default_path):
+    global _TONYISMS
 
-def main():
-	page = requests.get('http://101tonyisms.wordpress.com/').text
-	print page
+    if _TONYISMS is None:
+        with open(source) as f:
+            _TONYISMS = json.load(f) 
+
+    return _TONYISMS
+
+
+def random():
+    tonyisms = get()
+    choice = rand.randint(0, len(tonyisms)-1)
+    return tonyisms[choice]
+
 
 if __name__ == '__main__':
-	main()
+    print random()
 
